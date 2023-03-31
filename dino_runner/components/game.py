03a8e6e7800ccb5651abf8dcc89ab4846def4225
Adven6 <advric6@gmail.com>
2023-03-30 23:38:05 -0500

    Primera clase

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,6 @@
 import pygame
-
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.components.dinosaur import Dinosaur 
 
 
 class Game:
@@ -13,7 +13,8 @@ class Game:
         self.playing = False
         self.game_speed = 20
         self.x_pos_bg = 0
-        self.y_pos_bg = 380
+        self.y_pos_bg = 500
+        self.player = Dinosaur()
 
     def run(self):
         # Game loop: events - update - draw
@@ -36,9 +37,13 @@ class Game:
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
+# El blit sirve para agregar una imagen, 
+# desdes de agregarla se les pone unas cordenadas en el plano del juego
+    
     def draw_background(self):
         image_width = BG.get_width()
         self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
