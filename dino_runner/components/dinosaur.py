3148e57287029a39824be0133e26492ab4261bc2
Adven6 <advric6@gmail.com>
2023-04-02 20:02:00 -0500

    Tarea clase 2
    Entrega de los trabajos con demora por fallas de internet en mi hogar, desde el vienes en la tarde no he podido tener internet hasta hoy ya que con las lluvias la linea cerca de mi casa se daño

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -2,7 +2,7 @@ import pygame
 
 from pygame.sprite import Sprite
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 
 
@@ -10,6 +10,7 @@ class Dinosaur(Sprite):
     X_POS = 50
     Y_POS = 420
     JUMP_SPEED = 8.5
+    DUCK_SPEED = 8.5
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -19,12 +20,16 @@ class Dinosaur(Sprite):
         self.step_index = 0
         self.dino_jump = False
         self.jump_speed = self.JUMP_SPEED
+        self.dino_duck = False
+        self.duck_speed = self.DUCK_SPEED
         
     def update(self, user_input):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.dino_jump()  
+        elif self.dino_duck:
+            self.dino_duck()    
             
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
@@ -32,14 +37,27 @@ class Dinosaur(Sprite):
         elif not self.dino_jump:  
             self.dino_jump = False
             self.dino_run = True  
+            
+        
+        if user_input[pygame.K_DOWN] and not self.dino_duck:
+            self.dino_duck = True
+            self.dino_run = False    
+            self.dino_jump = False
+        elif not self.dino_duck:
+            self.dino_duck = False
+            self.dino_run = True
+        
         
         if self.step_index > 10:
             self.step_index = 0    
             
+        
+            
             
         
     def draw(self, screen):
-        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))    
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+            
         
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
@@ -57,4 +75,13 @@ class Dinosaur(Sprite):
             self.dino_jump = False
             self.jump_speed = self.JUMP_SPEED
             
+    def duck(self):
+        self.image = DUCKING
+        self.dino_rect.y = self.duck_speed*4
+        if self.duck_speed < -self.DUCK_SPEED:
+            self.dino_rect.y = self.Y_POS
+            self.dino_duck = False
+            self.duck_speed = self.DUCK_SPEED
+        
+        
             
